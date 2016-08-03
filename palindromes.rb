2aab2861d84dfb29d3bacc2c2758516ef99a0f44
Liam Willey <wwilleymc@gmail.com>
2016-08-03 14:24:38 -0700

    weekend 2 work

---
+++ palindromes.rb
@@ -0,0 +1,33 @@
+def palindrome?(string)
+  string == string.reverse
+
+end
+
+
+# Driver code - don't touch anything below this line.
+puts "TESTING palindrome?"
+puts
+
+result = palindrome?("racecar")
+
+puts "Your method returned:"
+p result
+puts
+
+if result == true
+  puts "PASS!"
+else
+  puts "F"
+end
+
+result = palindrome?("wazzzzup")
+
+puts "Your method returned:"
+p result
+puts
+
+if result == false
+  puts "PASS!"
+else
+  puts "F"
+end
---
+++ split_arrays.rb
@@ -0,0 +1,43 @@
+# Complete the method called split_array, which two parameters, the first being an array, and the second being an integer. The method will then create an array of other arrays, each one being the size of the indicated by the second parameter. See the driver code for examples.
+
+def split_array(array, number)
+  new_array = []
+  while split_array.any? do number
+    new_array << split_array.shift
+  end
+  return new_array
+end
+
+
+
+
+
+
+
+# Driver code - don't touch anything below this line.
+puts "TESTING split_array..."
+puts
+
+result = split_array([0, 1, 2, 3, 4, 5], 2)
+
+puts "Your method returned:"
+p result
+puts
+
+if result == [[0, 1], [2, 3], [4, 5]]
+  puts "PASS!"
+else
+  puts "F"
+end
+
+result = split_array([0, 1, 2, 3, 4, 5], 3)
+
+puts "Your method returned:"
+p result
+puts
+
+if result == [[0, 1, 2], [3, 4, 5]]
+  puts "PASS!"
+else
+  puts "F"
+end
